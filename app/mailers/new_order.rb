5c13d04e5ed6093a1b26eacdd627a10e54b2a160
HanaHsu <kobanae@gmail.com>
2013-03-15 11:45:08 +0800

    完成訂單後寄信到指定信箱

--- app/mailers/new_order.rb
+++ app/mailers/new_order.rb
@@ -4,7 +4,7 @@ class NewOrder < ActionMailer::Base
   def send_order(order)
   	@order = order
 
-  	mail(:to => [@order.order_email, "carolkao@longtermyeast.com", "service@longtermyeast.com", "kobanae@summers.com.tw"],
+  	mail(:to => [@order.order_email,  "kobanae@summers.com.tw"],
   	     :subject => "[榮騰網站]來自#{@order.order_name}的訂單")
   	
   end
