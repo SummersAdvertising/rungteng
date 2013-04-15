24faa59b5b1248da89f34cbbd97d9cb6abe170e9
HanaHsu <kobanae@gmail.com>
2013-04-15 14:51:31 +0800

    修正運費顯示

--- app/mailers/new_order.rb
+++ app/mailers/new_order.rb
@@ -4,7 +4,7 @@ class NewOrder < ActionMailer::Base
   def send_order(order)
   	@order = order
 
-  	mail(:to => [@order.order_email, "carolkao@longtermyeast.com", "service@longtermyeast.com"],
+  	mail(:to => [@order.order_email],
   	     :subject => "[夏天廣告測試信件]榮騰農產：來自#{@order.order_name}的訂單")
   	
   end
