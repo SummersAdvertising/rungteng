0ae1865d4ea57b74bc3c3cc3670035fd01f3d35a
HanaHsu <kobanae@gmail.com>
2013-04-16 17:45:24 +0800

    修正寄信錯誤

--- app/mailers/new_order.rb
+++ app/mailers/new_order.rb
@@ -4,8 +4,6 @@ class NewOrder < ActionMailer::Base
   def send_order(order)
   	@order = order
 
-    mail(:to => [@order.order_email, "carolkao@longtermyeast.com", "customerservice@longtermyeast.com"],
-  	#mail(:to => ["yuzhe@summers.com.tw"],
-  	     :subject => "榮騰農產：來自#{@order.order_name}的訂單")  	
+    mail(:to => [@order.order_email, "carolkao@longtermyeast.com", "customerservice@longtermyeast.com"], :subject => "榮騰農產：來自#{@order.order_name}的訂單")  	
   end
 end
