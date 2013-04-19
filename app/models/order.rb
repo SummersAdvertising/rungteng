6168a909429c577c1b41d9c9fddb668d3d3a5a50
HanaHsu <kobanae@gmail.com>
2013-04-19 17:48:03 +0800

    validate order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,8 @@
 class Order < ActiveRecord::Base
   attr_accessible :order_companytaxID, :order_email, :order_name, :order_phone, :order_fax, :receive_address, :receive_date, :receive_name, :receive_phone, :receive_time, :totalprice, :shippingfee, :note
-  #validates :order_companytaxID, :order_email, :order_name, :order_phone, :receive_address, :receive_date, :receive_name, :receive_phone, :receive_time, :totalprice, :presence => true
+  validates :order_companytaxID, :order_email, :order_name, :order_phone, :receive_address, :receive_date, :receive_name, :receive_phone, :receive_time, :totalprice, :presence => true
+
+  validates_numericality_of :totalprice, :greater_than => 0
 
   paginates_per 15
   
