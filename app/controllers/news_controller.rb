54c2408f6e8411c45a8f379a6641fc81b4760295
HanaHsu <kobanae@gmail.com>
2013-05-03 13:21:48 +0800

    修正文章無內容時報錯

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -16,25 +16,27 @@ class NewsController < ApplicationController
   end
   def show
     @news = News.find(params[:id])
+    
+    if(!@news.content.blank?)
+      @content = JSON.parse(@news.content)
+      @paragraphs = @content["article"]
 
-    @content = JSON.parse(@news.content) if !@news.content.nil?
-    @paragraphs = @content["article"]
+      $meta_description = '';
+      max_length = 150
+
+      # 產出 og:description
+      @paragraphs.each do | p |
+        next if p["type"] != "paragraph"
+
+        partical_content = p["content"].gsub(/<br([ \/]*)>/, "\n")
+        $meta_description += partical_content
+        max_length -= partical_content.length
+
+        break if max_length <= 0
+      end
+    end
 
     $meta_title = @news.name
-
-    $meta_description = '';
-    max_length = 150
-
-    # 產出 og:description
-    @paragraphs.each do | p |
-      next if p["type"] != "paragraph"
-
-      partical_content = p["content"].gsub(/<br([ \/]*)>/, "\n")
-      $meta_description += partical_content
-      max_length -= partical_content.length
-
-      break if max_length <= 0
-    end
 
     respond_to do |format|
       format.html # show.html.erb
